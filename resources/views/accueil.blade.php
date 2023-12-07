1464368b0ab855ef93f34b6ce272b9daa08e48c7
Theonaw <theonawro@gmail.com>
2023-12-07 15:28:25 +0100

    12eme commit

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -3,7 +3,9 @@
 
 @section('content')
 <h2>Accueil</h2>
-<img src="/img/danielle-cerullo-CQfNt66ttZM-unsplash.jpg" alt="photo 1">
-<img src="/img/mark-bertulfo-XKimW0pke6w-unsplash.jpg" alt="photo 2">
-<h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo molestiae voluptatem sit harum atque, sed, hic quaerat repellat illo et aspernatur architecto sequi nostrum quae debitis natus necessitatibus, pariatur laboriosam!</h3>
+<ul class="plats">
+    <img src="/img/danielle-cerullo-CQfNt66ttZM-unsplash.jpg" alt="photo 1">
+    <img src="/img/mark-bertulfo-XKimW0pke6w-unsplash.jpg" alt="photo 2">
+    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo molestiae voluptatem sit harum atque, sed, hic quaerat repellat illo et aspernatur architecto sequi nostrum quae debitis natus necessitatibus, pariatur laboriosam!</h3>
+</ul>
 @endsection
